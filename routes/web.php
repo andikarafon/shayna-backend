f8df19bed1b050dba6d564371988513bb4e3ee2c
andikarafon <andika.rafon.senohaji@gmail.com>
2022-12-13 10:26:40 +0700

    commit pertama 13 desember

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// jika menggunakan Laravel 8 atau 9. maka penulisan route nya ada perubahan
+Route::get('/', 'App\Http\Controllers\DashboardController@index');
+
+
